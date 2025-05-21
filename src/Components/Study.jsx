977e4efbd469df0be9612d1236386004782a5d38
gabriel <s2gb77@gamil.com>
2025-05-20 22:10:54 -0400

    edit

--- src/Components/Study.jsx
+++ src/Components/Study.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Study = () => {
   return (
-    <div className='flex mb-12 flex-col justify-center items-center gap-20 px-10 md:px-32'>
+    <div className='flex mt-10 mb-8 flex-col justify-center items-center gap-20 px-10 md:px-32'>
       <h1 className='text-center uppercase font-bold text-2xl'>Oque você irá aprender na <span className='text-cyan-500'>Tráfego Academy</span>?</h1>
       <div className='flex flex-col md:flex-row items-center justify-between w-full'>
         <img className='w-64 md:mb-12' src="./meta-ads.png" alt="" />
