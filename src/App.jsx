eff9ee2c6fddde6bb22071c76a58b5a4642c5afd
gabriel <s2gb77@gamil.com>
2025-05-20 22:07:43 -0400

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Hero from './Components/Hero'
 import Results from './Components/Results'
 import Study from './Components/Study'
-import Pricing from './Components/pricing'
+import Pricing from './Components/Pricing'
 import Footer from './Components/Footer'
 
 
